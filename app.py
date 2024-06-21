d74e50b6841bd8ddb6d40246be1bcb2b0fb931ec
Flavio Cesar(Web-Programmer) <119898588+FlavioC30@users.noreply.github.com>
2024-06-21 19:03:53 +0000

    commit4

--- app.py
+++ app.py
@@ -3,125 +3,49 @@ import streamlit as st
 from streamlit_lottie import st_lottie
 from PIL import Image
 
+# Interactive Streamlit elements, like these sliders, return their value.
+# This gives you an extremely simple interaction model.
+iterations = st.sidebar.slider("Level of detail", 2, 20, 10, 1)
+separation = st.sidebar.slider("Separation", 0.7, 2.0, 0.7885)
 
-#config
-st.set_page_config(page_title="Valerapp", page_icon="🤖", layout="wide")
+# Non-interactive elements return a placeholder to their location
+# in the app. Here we're storing progress_bar to update it later.
+progress_bar = st.sidebar.progress(0)
 
-def load_lottieurl(url):
-    r = requests.get(url)
-    if r.status_code !=200:
-        return None
-    return r.json()
+# These two elements will be filled in later, so we create a placeholder
+# for them using st.empty()
+frame_text = st.sidebar.empty()
+image = st.empty()
 
-def local_css(file_name):
-    with open(file_name) as f:
-        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+m, n, s = 960, 640, 400
+x = np.linspace(-m / s, m / s, num=m).reshape((1, m))
+y = np.linspace(-n / s, n / s, num=n).reshape((n, 1))
 
-local_css("style/style.css")
-email_address ="emailcontact@gmail.com"
+for frame_num, a in enumerate(np.linspace(0.0, 4 * np.pi, 100)):
+    # Here were setting value for these two elements.
+    progress_bar.progress(frame_num)
+    frame_text.text("Frame %i/100" % (frame_num + 1))
 
-lottie_file ="https://assets9.lottiefiles.com/packages/lf20_ggwq3ysg.json"
+    # Performing some fractal wizardry.
+    c = separation * np.exp(1j * a)
+    Z = np.tile(x, (n, 1)) + 1j * np.tile(y, (1, m))
+    C = np.full((n, m), c)
+    M: Any = np.full((n, m), True, dtype=bool)
+    N = np.zeros((n, m))
 
-with st.container():
-    st.subheader("Hola, somos Valerapp :wave:")
-    st.title("Creamos soluciones para acelerar tu negocio")
-    st.write(
-        "Somos unos apasionados de la tecnología y la innovación, especializados en el sector de la digitalización y automatización de negocios. Nos gusta crear soluciones para resolver problemas y mejorar procesos."
-    )
-    st.write("[Saber más >](https://valerapp.com/)")
+    for i in range(iterations):
+        Z[M] = Z[M] * Z[M] + C[M]
+        M[np.abs(Z) > 2] = False
+        N[M] = i
 
+    # Update the image placeholder by calling the image() function on it.
+    image.image(1.0 - (N / N.max()), use_column_width=True)
 
+# We clear elements by calling empty on them.
+progress_bar.empty()
+frame_text.empty()
 
-# sobre nosotros
-with st.container():
-    st.write("---")
-    left_column, right_column= st.columns((2))
-    with left_column:
-        st.header("Sobre nosotros")
-        st.write(
-            """
-            Nuestro objetivo es poder aportar valor a los negocios ayudandoles a ahorrar tiempo y dinero a través de la implantación de nuevas tecnologías como la inteligencia artifical, analisis de datos o implantación de software de automatización.
-            Seguramente te vamos a poder ayudar si:
-
-            - Tienes un negocio y quieres mejorar tus procesos de trabajo para ahorrar tiempo y dinero
-            - Tienes trabajadores que emplean parte de su jornada a realizar tareas repetitivas sin valor añadido para tu negocio
-            - No tienes claras las métricas de tu negocio y quieres tomar decisiones basadas en datos
-            - Quieres mejorar la experiencia de tus clientes
-            - Usas herramientas de software antiguas o poco eficientes o procesos en los que usas papel y bolígrafo
-
-            ***Si esto suena interesante para ti puedes contactarnos a través del formulario que encontrarás al final de la página*** 
-            """
-        )
-        st.write("[Más sobre nosotros>](https://valerapp.com/about/)")
-    with right_column:
-        st_lottie(load_lottieurl(lottie_file), height=400)
-
-# servicios
-with st.container():
-    st.write("---")
-    st.header("Nuestros servicios")
-    st.write("##")
-    image_column, text_column = st.columns((1,2))
-    with image_column:
-        image = Image.open("images/apps.png")
-        st.image(image, use_column_width=True)
-    with text_column:
-        st.subheader("Diseño de aplicaciones")
-        st.write(
-            """
-            Si en tus procesos diarios tienes que introducir información en diferentes fuentes de datos o bien tienes que trabajar con documentación en papel, es hora de pensar en implementar una aplicación en tu negocio para potenciar y optimizar el funcionamiento de los procesos diarios.    
-            """
-        )
-        st.write("[Ver servicios >](https://valerapp.com/services/)")
-
-with st.container():
-    st.write("---")
-    st.write("##")
-    image_column, text_column = st.columns((1,2))
-    with image_column:
-        image = Image.open("images/automation.png")
-        st.image(image, use_column_width=True)
-    with text_column:
-        st.subheader("Automatización de procesos")
-        st.write(
-            """
-            Si realizas cualquier tipo de tarea repetitiva como por ejemplo introducir datos en excel u otras aplicaciones, gestión de facturas, envío de emails a proveedores etc Lo que quizás necesitas es una automatización de tareas para poder liberar recursos de esas actividades y poder emplearlos en otras tareas más productivas.
-            """
-        )
-        st.write("[Ver servicios >](https://valerapp.com/services/)")
-
-with st.container():
-    st.write("---")
-    st.write("##")
-    image_column, text_column = st.columns((1,2))
-    with image_column:
-        image = Image.open("images/visualizacion.png")
-        st.image(image, use_column_width=True)
-    with text_column:
-        st.subheader("Visualización de datos")
-        st.write(
-            """
-            Si sientes que no tienes una visión clara de datos de tu negocio lo que necesitas es una aplicación en la que puedas tener toda la información de interes de tu empresa.
-            """
-        )
-        st.write("[Ver servicios >](https://valerapp.com/services/)")
-
-# contacto
-with st.container():
-    st.write("---")
-    st.header("Ponte en contacto con nosotros!")
-    st.write("##")
-    contact_form = f"""
-    <form action="https://formsubmit.co/{email_address}" method="POST">
-        <input type="hidden" name="_captcha" value="false">
-        <input type="text" name="name" placeholder="Tu nombre" required>
-        <input type="email" name="email" placeholder="Tu email" required>
-        <textarea name="message" placeholder="Tu mensaje aquí" required></textarea>
-        <button type="submit">Enviar</button>
-    </form>
-    """
-    left_column, right_column = st.columns(2)
-    with left_column:
-        st.markdown(contact_form, unsafe_allow_html=True)
-    with right_column:
-        st.empty()
+# Streamlit widgets automatically run the script from top to bottom. Since
+# this button is not connected to any other logic, it just causes a plain
+# rerun.
+st.button("Re-run")
